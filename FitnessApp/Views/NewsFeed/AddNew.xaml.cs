377d0bbb5de6a13a603510056a313603fe1594be
NiE23 <nikeri@gmx.de>
2020-11-10 12:11:13 +0100

    Einstellungen

---
+++ FitnessApp/Views/NewsFeed/AddNew.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessApp
+{
+    public partial class AddNew : ContentPage
+    {
+        public AddNewVM AddNewVM { get; set; }
+
+        public AddNew()
+        {
+            InitializeComponent();
+            AddNewVM = new AddNewVM();
+            Start();
+            BindingContext = AddNewVM;
+        }
+
+        private void Loaded(object sender, EventArgs e)
+        {
+            listOptions.SelectedItem = null;
+        }
+
+        private void Start()
+        {
+            Title = "Hinzufügen";
+        }
+
+        async void Selection(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            string msg = null;
+            ListView listView = (sender as ListView);
+
+            if (listView != null && listView.SelectedItem != null)
+            {
+                string result = listView.SelectedItem.ToString();
+                msg = result;
+
+                switch (result)
+                {
+                    case "Status":
+                        await this.Navigation.PushAsync(new StatusNew());
+                        break;
+                    default:
+                        await DisplayAlert("Auswahl:", msg, "OK");
+                        break;
+                }
+                listView.SelectedItem = null;
+            }
+        }
+    }
+}
