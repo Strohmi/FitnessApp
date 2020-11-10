df93e93a8823d9e32a7f93b54a770fd0a4967723
NiE23 <nikeri@gmx.de>
2020-11-10 14:45:39 +0100

    Push-Ups

---
+++ FitnessApp/Models/General/NotifyPropertyBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FitnessApp
+{
+    public class NotifyPropertyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged<T>(ref T variable, T value, [CallerMemberName] string propertyName = null)
+        {
+            variable = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
