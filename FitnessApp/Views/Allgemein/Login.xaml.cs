b9296776f915ae6a29211460618b4e226ffb9ae1
Nico <65763064+Nico258@users.noreply.github.com>
2020-11-14 14:00:15 +0100

    Login (unfertig)

---
+++ FitnessApp/Views/Allgemein/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Linq;
+using FitnessApp.Models;
+
+namespace FitnessApp
+{
+    public partial class Login : ContentPage
+    {
+
+        public string Nutzername { get; set; }
+        public string Passwort { get; set; }
+
+        private string hashedpw;
+
+        public Login()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            _ = Nutzername;
+        }
+
+    }
+}
