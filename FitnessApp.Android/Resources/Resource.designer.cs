1a593c69837975e2093e66e9d205de7dd936018f
NiE23 <nikeri@gmx.de>
2020-11-16 21:49:22 +0100

    Profil Anpassungen 2

--- FitnessApp.Android/Resources/Resource.designer.cs
+++ FitnessApp.Android/Resources/Resource.designer.cs
@@ -8752,22 +8752,19 @@ namespace FitnessApp.Droid
 			public const int Plus = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int ProfilBildDefault = 2131165303;
+			public const int tab_about = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int tab_about = 2131165304;
+			public const int tab_feed = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int tab_feed = 2131165305;
+			public const int tooltip_frame_dark = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int tooltip_frame_dark = 2131165306;
+			public const int tooltip_frame_light = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tooltip_frame_light = 2131165307;
-			
-			// aapt resource value: 0x7F07007C
-			public const int xamarin_logo = 2131165308;
+			public const int xamarin_logo = 2131165307;
 			
 			static Drawable()
 			{
