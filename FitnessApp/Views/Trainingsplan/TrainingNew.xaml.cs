2fd99b541121d139e9bc6ac2a0363e6540e38cf3
Strohmi <73481045+Strohmi@users.noreply.github.com>
2020-11-18 16:44:55 +0100

    Nur das Modell für den Trainingsplan Hintergrund fehlt noch

--- FitnessApp/Views/Trainingsplan/TrainingNew.xaml.cs
+++ FitnessApp/Views/Trainingsplan/TrainingNew.xaml.cs
@@ -8,19 +8,32 @@ namespace FitnessApp
 {
     public partial class TrainingNew : ContentPage
     {
-        public TrainingVM TrainingVM { get; set; }
-
         public TrainingNew()
         {
             InitializeComponent();
             TrainingVM = new TrainingVM();
+            Uebungen = new List<string>();
+            Repetitions = new List<int>();
+            Sets = new List<int>();
+            Weights = new List<decimal>();
             Start();
             BindingContext = TrainingVM;
         }
 
+        public TrainingVM TrainingVM { get; set; } 
+        public List<string> Uebungen { get; set; }
+        public List<int> Repetitions { get; set; }
+        public List<int> Sets { get; set; }
+        public List<decimal> Weights { get; set; }
+
         private void Start()
         {
-            Title = "Hinzufügen";
+            Title = "Trainingsplan erstellen";
+        }
+
+        private void Add(object sender, EventArgs e)
+        {
+            
         }
     }
 }
