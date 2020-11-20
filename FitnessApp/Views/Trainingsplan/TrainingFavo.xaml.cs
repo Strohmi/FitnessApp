2e98f00b5bb55af3c38f54fec84b7420b10468c6
NiE23 <nikeri@gmx.de>
2020-11-20 21:32:55 +0100

    Neue Icons

---
+++ FitnessApp/Views/Trainingsplan/TrainingFavo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessApp
+{
+    public partial class TrainingFavo : ContentPage
+    {
+        public TrainingFavo()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            Title = "Favoriten";
+        }
+
+        private void GetList()
+        {
+
+        }
+    }
+}
